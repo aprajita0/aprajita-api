3cf68a3949c77ec6f71f6238322e6346d469ff69
Aprajita Srivastava <98621004+aprajita0@users.noreply.github.com>
2024-07-03 13:46:01 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Music = require('./models/musicModel')
+/*const Music = require('./models/musicModel')*/
 const app = express()
 const cors = require('cors')
 
